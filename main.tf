2df8858649c6a215bdea43581e10f40e2779e840
jb <jbwlab@gmail.com>
2025-01-30 18:00:35 +0100

    main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.71"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "~> 3.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
