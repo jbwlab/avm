7fd68eb214a2995aa761f283acc7029ec452877b
jb <jbwlab@gmail.com>
2025-02-20 18:31:07 +0100

    added tfstate to az storage

--- terraform.tf
+++ terraform.tf
@@ -10,7 +10,17 @@ terraform {
       version = "~> 3.6"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name = "rg-lab-terraform"
+    storage_account_name = "stlabtfst"
+    container_name = "tfstate"
+    key = "avm-lab.tfstate"
+    //se_azuread_auth = true
+  }
 }
+
+
 
 provider "azurerm" {
   features {
