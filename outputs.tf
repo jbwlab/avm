4264d28de4c16e8185e96a6cc0829194f53419e0
jb <jbwlab@gmail.com>
2025-02-22 19:31:19 +0100

    base_develop

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "names" {
   value = {
     resource_group_name        = module.resource_group.name
-    log_analytics_workspace = module.log_analytics_workspace.id
+    log_analytics_workspace_id = module.log_analytics_workspace.id
   }
 }
